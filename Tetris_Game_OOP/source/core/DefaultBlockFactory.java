426f16454a62188ad8d3992d386dc9dd52d7a21d
nammais <nguyenmainam28@gmail.com>
2025-11-09 00:47:09 +0700

    tạo class sinh khối

---
+++ Tetris_Game_OOP/source/core/DefaultBlockFactory.java
@@ -0,0 +1,25 @@
+package core;
+
+import core.Config;
+import java.util.Random;
+public class DefaultBlockFactory implements BlockFactory {
+    private final Random RAND = new Random();
+
+    @Override
+    public Block createRandom() {
+        // spawn ở hàng 0, canh giữa theo số cột hiện có 
+        int row = 0;
+        int col = Config.COLS / 2 - 2; // bớt 2 để khối 4 ô không bị lệch phải
+        int r = RAND.nextInt(7);
+
+        switch (r) {
+            case 0: return new blocks.IBlock(row, col);
+            case 1: return new blocks.JBlock(row, col);
+            case 2: return new blocks.LBlock(row, col);
+            case 3: return new blocks.OBlock(row, col);
+            case 4: return new blocks.SBlock(row, col);
+            case 5: return new blocks.TBlock(row, col);
+            case 6: return new blocks.ZBlock(row, col);
+        }
+    }
+}   
